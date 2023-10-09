b10c917e041b8e3fdd59349d71c1195668873ea0
JiSeok <jiseok7620@naver.com>
2023-10-09 10:02:45 +0900

    feat: Added overseas futures daily transaction search function

--- ebestapi/overseas_futures.py
+++ ebestapi/overseas_futures.py
@@ -32,6 +32,36 @@ class OverseesFuturesCls:
         print(result)
         return result
 
+    # 해외선물 일별체결 조회
+    def overseas_futures_daily_trading_inquiry(self, ACCESS_TOKEN):
+        # 요청 url
+        PATH = "overseas-futureoption/market-data"
+        URL = f"{self.BASE_URL}/{PATH}"
+
+        # 요청 header
+        header = {
+            "content-type":"application/json; charset=utf-8",
+            "authorization": f"Bearer {ACCESS_TOKEN}",
+            "tr_cd":"o3104",
+            "tr_cont":"N",
+            "tr_cont_key":"",
+        }
+
+        # body
+        body = {
+          "o3104InBlock": {
+            "gubun": "0",
+            "shcode": "CUSV23",
+            "date": "20230908"
+          }
+        }
+
+        # 요청 보내기
+        request = requests.post(URL, headers=header, data=json.dumps(body))
+        result = request.json()
+        print(result)
+        return result
+
     # 해외선물차트 분봉 조회
     def check_overseas_futures_charts(self, ACCESS_TOKEN):
         # 요청 url
